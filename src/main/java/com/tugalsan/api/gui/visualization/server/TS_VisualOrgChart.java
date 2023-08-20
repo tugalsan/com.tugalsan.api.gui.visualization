18faef0ab8a4d9d3c6bd5a9cc9210b5fe850d96b
tugalsan karabacak <tugalsan@gmail.com>
2023-08-20 17:50:59 +0300

    .

--- src/main/java/com/tugalsan/api/gui/visualization/server/TS_VisualOrgChart.java
+++ src/main/java/com/tugalsan/api/gui/visualization/server/TS_VisualOrgChart.java
@@ -8,6 +8,11 @@ public class TS_VisualOrgChart {
 
     public static StringBuilder preScript(StringBuilder sb) {
         return sb.append("""
+                    <style>
+                        table{
+                            border-collapse: seperate;
+                        }
+                    </style>
                     <script type='text/javascript' src='https://www.google.com/jsapi'></script>
                     <script type='text/javascript'>
                     google.load('visualization', '1', {packages:['orgchart']});
